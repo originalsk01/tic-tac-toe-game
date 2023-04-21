166c3f0af3cd0e1e50c3800c3424aba4e47855e9
orginalsk01 <sipssman@gmail.com>
2023-04-21 19:52:00 +0200

    Initial Commit

---
+++ scripts/config.js
@@ -0,0 +1,36 @@
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid; // +'1'=> 1
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playerName").trim(); // '    SIpho SK    ' => 'Sipho SK'
+
+  if (!enteredPlayerName) {
+    // enteredPlayerNName === ""
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid player name!";
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
+}
